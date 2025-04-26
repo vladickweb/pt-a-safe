5200b5c6d5e93ff29ba39108ad15c027285ca2d0
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-04-26 20:04:56 +0200

    chore: add ESLint configuration with Prettier integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
