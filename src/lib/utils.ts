592c9cfce6314bbf723f58f2b38c8c98f7ed3d16
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-04-27 19:45:16 +0200

    feat: refactor change function by arrow function in tils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,11 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export function getChangeColor(change: number) {
+export const getChangeColor = (change: number) => {
   return change >= 0 ? "text-green-500" : "text-red-500";
-}
+};
 
-export function formatChangeValue(change: number) {
+export const formatChangeValue = (change: number) => {
   const prefix = change >= 0 ? "+" : "";
   return `${prefix}${change}%`;
-}
+};
