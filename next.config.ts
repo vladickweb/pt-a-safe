0c89293a5952ae56fffe24131561581ab7d349fa
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-04-29 17:54:46 +0200

    feat: try to fix next error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+    // Instead, use `not-found.ts` as a workaround
+    // "ts" is required to resolve `not-found.ts`
+    // https://github.com/vercel/next.js/issues/65447
+    "ts",
+  ],
 };
 
 export default nextConfig;
